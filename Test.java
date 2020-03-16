86511074f96cecac9b4d987fdebfd616c781709a
SupremeEthan <43837579+SupremeEthan@users.noreply.github.com>
2020-03-16 19:32:42 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,12 @@
 /**
 * This was created by
 * Tim Hickey
+* 
 */
 public class Test {
   public static void main(String[] args){
     System.out.println("Konichi-wa");
     System.out.println("Nicole");
+    System.out.println("happy coding!!!");
   }
 }
