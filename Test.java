62af15de90faf90302541554d6dc232b9557dd44
Timothy J Hickey <tjhickey@brandeis.edu>
2020-03-16 19:33:34 -0400

    added a comment to main method

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@
 * Tim Hickey
 */
 public class Test {
+  /** this prints a greeting -- Tim **/
   public static void main(String[] args){
     System.out.println("Konichi-wa");
     System.out.println("Nicole");
